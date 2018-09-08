6256da8c0970e00d6e1eccbfa2673200ee9e248e
chadwishner <chadwishner@gmail.com>
2018-09-08 19:29:45 -0400

    Finished

--- listingController.js
+++ listingController.js
@@ -2,13 +2,32 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    document.getElementById("detailedInformation").style.display = "none";
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+    	var listingToAdd =
+    	{
+            "code": $scope.code, 
+            "name": $scope.name, 
+            "coordinates": {
+                "latitude": $scope.latitude, 
+                "longitude": $scope.longitude
+            }, 
+            "address": $scope.address
+        }; 
+
+        $scope.listings.push(listingToAdd);
+        $scope.listings.sort(function(a,b) {return (a.code > b.code) ? 1 : ((b.code > a.code) ? -1 : 0);});
+        document.getElementById("form").reset();
+
+    };
+    $scope.deleteListing = function(index) {
+    	$scope.listings.splice(index, 1);
+
+    };
+    $scope.showDetails = function(index) {
+    	document.getElementById("detailedInformation").style.display = "block";
+    	$scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
